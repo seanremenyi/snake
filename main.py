be11f50c52a0ba04166df4a99dfa90acbee55c33
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-05 09:16:08 +1000

    added requirements

--- main.py
+++ main.py
@@ -36,12 +36,12 @@ while game_is_on:
         scoreboard.increase_score()
 
     #Detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -300 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
 
     #Detect collision with tail.
-    for segment in snake.segments[1:-1]:
+    for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
