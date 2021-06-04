d7082b88a9744a7fe26b0415cf97aeda39d7330f
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-04 20:27:24 +1000

    OOP turned snake into a module

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,23 +9,15 @@ screen.bgcolor("black")
 screen.title("My Python Game")
 screen.tracer(0)
 
-snake_body = []
-x_positions = [0, -20, -40]
 
-for turtles in range(0,3):
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.goto(x=x_positions[turtles], y=0)
-    snake_body.append(new_turtle)
+snake = Snake()
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment in snake_body:
-        segment.forward(20)
 
+    snake.move()
 
 screen.exitonclick()
